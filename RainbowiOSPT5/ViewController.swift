d7eec394b3bc6a19065ae77f9f0546aee1f4d9a4
David Williams <burmesedragon@att.net>
2020-01-28 18:53:13 -0800

    Getting started

---
+++ RainbowiOSPT5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowiOSPT5
+//
+//  Created by David Williams on 1/28/20.
+//  Copyright © 2020 david williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
